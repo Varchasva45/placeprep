e70c1bdd06ce067433964fc784f54ee55147244d
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-03-27 16:03:25 +0530

    Updated interviewSchema

--- server/src/models/interviewSchema.ts
+++ server/src/models/interviewSchema.ts
@@ -1,21 +1,66 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 import getDb from "./db";
 
 interface Interview extends Document {
     interviewer: string;
-    interviewee: string;
+    intervieweeName: string;
+    intervieweeId: mongoose.Types.ObjectId;
+    position: string;
+    date: Date;
+    questions: string[];
+    evaluation: string[];
+    feedback: string;
 }
 
 const interviewSchema = new Schema<Interview>({
     interviewer: {
         type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: (value: string) => {
+                return value === "AI" || value === "String";
+            },
+            message: "Interviewer must be 'AI' or 'String'"
+        }
+    },
+    intervieweeName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    intervieweeId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        trim: true
+    },
+    position: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    questions: {
+        type: [String],
         required: true
     },
-    interviewee: {
+    evaluation: {
+        type: [String],
+        required: true
+    },
+    feedback: {
         type: String,
         required: true
     }
 });
 
-const InterviewModel = getDb('interview_db').model<Interview>("Interview", interviewSchema);
+function InterviewModel(intervieweeId: mongoose.Types.ObjectId): Model<Interview> {
+    const modelName = intervieweeId.toHexString(); 
+    return getDb('interview_db').models[modelName] || getDb('interview_db').model<Interview>(modelName, interviewSchema);
+}
+
 export default InterviewModel;
