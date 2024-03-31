71181f669c531a3d6f6362381e2c85e32331463c
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-04-01 01:29:17 +0530

    Updated Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC = () => {
     const path = location.pathname;
 
     return (
-        <div className='w-full max-w-maxContent bg-white flex py-3 justify-between shadow-lg'>
+        <div className='w-full max-w-maxContent shadow-lg bg-gray-300 flex py-3 justify-between'>
 
             <Link to="/" className='pl-4 font-bold'>
                 PlacePrep
@@ -19,18 +19,50 @@ const NavBar: React.FC = () => {
             <nav className='hidden lg:flex'>
                 <ul className='flex flex-row gap-8'>
                     <li>
-                        <a href="/interview" className={`${path === '/interview' ? 'bg-gray-200' : ''} rounded-2xl`}>Interview</a>
-                        <span><FontAwesomeIcon icon={faChevronDown} height={10} width={10} className='pl-1'/></span>
+                        <div className='group'>
+
+                            <a href="/interview" className={path === '/interview' ? 'bg-gray-200' : ''}>Interview</a>
+                            <span><FontAwesomeIcon icon={faChevronDown} height={10} width={10} className='pl-1'/></span>
+
+                            <div className='absolute invisible opacity-0 flex justify-center items-center w-5/12 h-2/5  bg-blue-100 rounded-xl shadow-lg translate-x-[-25%] translate-y-[15%] translate-z-[-20%] group-hover:visible group-hover:translate-y-[6%] group-hover:translate-z-[20%]  group-hover:opacity-100 transition-all duration-200'>
+                                <ul className='w-full h-full flex p-3'>
+                                    <li className='w-full h-full flex flex-col justify-center items-center hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">AI Interview</a>
+                                    </li>
+                                    <li className='w-full h-full flex flex-col justify-center items-center hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">SDE Interview</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </li>
                     <li>
                         <a href="/practice" className={path === '/practice' ? 'bg-gray-200' : ''}>Coding Practice</a>
                     </li>
                     <li>
-                        <div>
-                            <a href="/playground" className={path === '/playground' ? 'bg-gray-200' : ''}>Playground</a>
+                        <div className='group'>
+
+                            <a href="/playground" className={path === '/playground' ? 'bg-gray-300' : ''}>Playground</a>
                             <span><FontAwesomeIcon icon={faChevronDown} height={10} width={10} className='pl-1'/></span>
-                            <div className='z-[1000]'>
-                                DropDown List
+
+                            <div className='absolute invisible opacity-0 flex justify-center items-center bg-blue-100 rounded-xl shadow-lg w-[180px] translate-x-[-25%] translate-y-[15%] translate-z-[-20%] group-hover:visible group-hover:translate-y-[8%] group-hover:translate-z-[20%]  group-hover:opacity-100 transition-all duration-200'>
+                                <ul className='w-full text-center flex flex-col p-3'>
+                                    <li className='w-full hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">Python</a>
+                                    </li>
+                                    <li className='w-full hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">Java</a>
+                                    </li>
+                                    <li className='w-full hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">C++</a>
+                                    </li>
+                                    <li className='w-full hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">C</a>
+                                    </li>
+                                    <li className='w-full hover:bg-white rounded-lg p-2'>
+                                        <a href="/playground">JavaScript</a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </li>
