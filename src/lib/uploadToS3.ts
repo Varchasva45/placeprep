c829036141d96b3267bf4b4ad9e2f8cd4e879bab
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-06-06 04:07:16 +0530

    Updated AWS Secrets

--- src/lib/uploadToS3.ts
+++ src/lib/uploadToS3.ts
@@ -1,9 +1,9 @@
 import { S3 } from "@aws-sdk/client-s3";
 
-const accessKeyId = '';
-const secretAccessKey = '';
-const region = '';
-const bucketName = '';
+const accessKeyId = import.meta.env.VITE_AWS_ACCESS_KEY_ID;
+const secretAccessKey = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
+const region = import.meta.env.VITE_AWS_REGION;
+const bucketName = import.meta.env.VITE_AWS_BUCKET_NAME;
 
 const s3 = new S3({
   region: region,
